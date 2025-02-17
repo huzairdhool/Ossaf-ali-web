e4fb23c13142027f97d121c96c58b771ecf9e297
Huzair Asim <huzairdhool@gmail.com>
2025-02-17 12:25:22 +0500

    message

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
     event.preventDefault();
     const formData = new FormData(event.target);
 
-    formData.append("access_key", "aed7d26c-c179-480f-afb7-fc9138d8c7e0");
+    formData.append("access_key", "e22abcd3-c14f-453a-8a73-f020b798cbb2");
 
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
@@ -25,7 +25,7 @@ const Contact = () => {
 
     if (res.success) {
       setStatus({ type: "success", message: "Message sent successfully!" });
-      event.target.reset(); // Reset form
+      event.target.reset();
     } else {
       setStatus({ type: "error", message: "Something went wrong. Try again." });
     }
@@ -45,17 +45,17 @@ const Contact = () => {
                 <img src={assets.contact} className='w-5 mr-2'></img>
                   <span className='text-blue-400'>Call Us</span>
                 </div>
-                <p className="mb-4 font-medium">+92333-8431038</p>
+                <p className="mb-4 font-medium">+923338431038</p>
                 <div className="flex items-center mb-2">
                 <img src={assets.mail} className='w-5 mr-2'></img>
-                  <span className='text-blue-400'>Email Us</span>
+                  <span className='text-blue-400'>Email us</span>
                 </div>
-                <p className="mb-4 font-medium">example@mail.com</p>
+                <p className="mb-4 font-medium">osaafalipropertycenter@gmail.com</p>
                 <div className="flex items-center mb-2">
                 <img src={assets.loc_icon} className='w-5 mr-2'></img>
                   <span className='text-blue-400'>Address</span>
                 </div>
-                <p className='font-medium'>Rehman Shaheed road near Yousaf Plaza, Gujrat.</p>
+                <p className='font-medium'>Ossaf Ali Property center Rehman shaheed road, Gujrat</p>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3361.754816564537!2d74.0655312!3d32.5860623!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391f1b2b17f0ce6d%3A0xbd8d7b166bbbae91!2zT3NhZiBBbGkgUHJvcGVydHkgQ2VudGVyINin2YjYtdin2YEg2LnZhNuMINm-2LHYp9m-2LHZuduMINiz2YbZudix!5e0!3m2!1sen!2s!4v1738309605630!5m2!1sen!2s" width="auto" height="auto"
                 className='mt-3'></iframe>
               </div>
